2198eee97eef5d4d07a6d54c023aafb1dd403748
Nguyen Nhut Truong <truongnn@overate-vntech.com>
2026-07-05 16:31:46 +0700

    no message

--- com.truong.oauth/src/main/java/com/truong/oauth/TruongSpringBootApplication.java
+++ com.truong.oauth/src/main/java/com/truong/oauth/TruongSpringBootApplication.java
@@ -3,7 +3,6 @@ package com.truong.oauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class TruongSpringBootApplication extends SpringBootServletInitializer{
